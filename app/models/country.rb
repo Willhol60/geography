2bfeac64b362a1b6de513f8461f09561e70e5e0f
Will Holmes <willholmes60@gmail.com>
2022-02-22 12:07:08 +0000

    remove comments

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,6 @@
 class Country < ApplicationRecord
   has_many :wishes, dependent: :destroy
   has_many :visits, dependent: :destroy
-  # belongs_to :continent
 
   validates :name, :flag, :population, :capital, presence: true
   validates :population, numericality: true
